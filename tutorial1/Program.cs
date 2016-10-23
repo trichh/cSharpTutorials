ae894f09d090229be0d7e3e2d47038771d87a0d7
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-10-23 11:29:56 -0400

    First few tutorials for c#

---
+++ tutorial1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Variables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float price = 20.95f;
+            char character = 'A';
+            string sentence = "I love learning new code";
+            bool isWorking = false;
+            const float pi = 3.14f;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(price);
+            Console.WriteLine(character);
+            Console.WriteLine(sentence);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", number, count);
+            Console.WriteLine(pi);
+        }
+    }
+}
---
+++ tutorial2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            byte b = (byte) i;
+            int n = 1000;
+            byte c = (byte) n;
+            string number = "1234";
+            int num = Convert.ToInt32(number);
+            Console.WriteLine("NOT LOOSING DATA " + b);
+            Console.WriteLine("LOOSING DATA " + c);
+            Console.WriteLine("Converting string " + number + " to interger " + num);
+            try
+            {
+                byte d = Convert.ToByte(number);
+                Console.WriteLine("Converting string " + number + " to byte " + d);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte!");
+            }
+            try
+            {
+                string str = "true";
+                bool boolean = Convert.ToBoolean(str);
+                Console.WriteLine("Converting string " + str + " to boolean " + boolean);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The string could not be converted to a boolean!");
+            }
+        }
+    }
+}
